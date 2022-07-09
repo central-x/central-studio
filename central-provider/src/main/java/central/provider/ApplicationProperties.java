ef2b91621844687f45496ab492b180f134ecab35
Alan Yeh <alan@yeh.cn>
2022-07-10 04:11:05 +0800

    Initial

---
+++ central-provider/src/main/java/central/provider/ApplicationProperties.java
@@ -0,0 +1,8 @@
+package central.provider;
+
+/**
+ * @author Alan Yeh
+ * @since 2022/07/07
+ */
+public class ApplicationProperties {
+}
---
+++ central-provider/src/main/java/central/provider/ProviderApplication.java
@@ -0,0 +1,18 @@
+package central.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Provider Application
+ *
+ * @author Alan Yeh
+ * @since 2022/07/07
+ */
+@SpringBootApplication
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+}
