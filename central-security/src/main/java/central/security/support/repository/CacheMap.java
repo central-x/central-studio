71b4f234e342de2304fb427931855cd6eaf9de88
Alan Yeh <alan@yeh.cn>
2023-06-27 00:50:07 +0800

    [UPDATE] 更新 CacheMap 的接口声明
    Signed-off-by: Alan Yeh <alan@yeh.cn>

--- central-security/src/main/java/central/security/support/repository/CacheMap.java
+++ central-security/src/main/java/central/security/support/repository/CacheMap.java
@@ -24,6 +24,11 @@
 
 package central.security.support.repository;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 缓存键值对
  *
@@ -31,4 +36,86 @@ package central.security.support.repository;
  * @since 2023/06/10
  */
 public interface CacheMap {
+    /**
+     * 移除指定的键的值
+     *
+     * @param keys 待移除键
+     * @return 被删除的元素数量
+     */
+    long delete(String... keys);
+
+    /**
+     * 判断是否包含指定键的元素
+     *
+     * @param key 待判断键
+     * @return 是否包含
+     */
+    boolean hasKey(String key);
+
+    /**
+     * 获取指定键的元素
+     *
+     * @param key 待取元素键
+     * @return 已取出的元素
+     */
+    String get(String key);
+
+    /**
+     * 获取指定键的元素
+     *
+     * @param keys 待取元素键集合
+     * @return 已取出的元素
+     */
+    List<String> get(String... keys);
+
+    /**
+     * 获取指定键的元素
+     *
+     * @param keys 待取元素键集合
+     * @return 已取出的元素
+     */
+    List<String> get(Collection<String> keys);
+
+    /**
+     * 获取所有键
+     */
+    Set<String> keys();
+
+    /**
+     * 获取所有元素数量
+     */
+    long size();
+
+    /**
+     * 保存指定值
+     *
+     * @param key   键
+     * @param value 值
+     */
+    void put(String key, String value);
+
+    /**
+     * 保存指定的键的值
+     *
+     * @param values 待保存的元素
+     */
+    void putAll(Map<String, String> values);
+
+    /**
+     * 如果当前 Map 不存在指定的键，则保存为新值
+     *
+     * @param map 待保存的元素
+     * @return 是否保存民功
+     */
+    boolean putIfAbsent(Map<String, String> map);
+
+    /**
+     * 获取所有元素
+     */
+    Set<String> values();
+
+    /**
+     * 获取整个 Map
+     */
+    Map<String, String> entries();
 }
