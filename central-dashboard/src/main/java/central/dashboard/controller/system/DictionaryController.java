c9cddc12c707887c44201a4edb2b62c192c21984
Alan Yeh <alan@yeh.cn>
2023-11-21 22:44:16 +0800

    [ADD] 新增系统配置相关控制器
    Signed-off-by: Alan Yeh <alan@yeh.cn>

---
+++ central-dashboard/src/main/java/central/dashboard/controller/system/DictionaryController.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022-present Alan Yeh <alan@yeh.cn>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package central.dashboard.controller.system;
+
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Dictionary Controller
+ * <p>
+ * 字典管理
+ *
+ * @author Alan Yeh
+ * @since 2023/11/21
+ */
+@RestController
+@RequiresAuthentication
+@RequiresPermissions(DatabaseController.Permissions.VIEW)
+@RequestMapping("/api/system/dictionaries")
+public class DictionaryController {
+
+    /**
+     * 权限
+     */
+    public interface Permissions {
+        String VIEW = "${application}:system:dictionary:view";
+        String ADD = "${application}:system:dictionary:add";
+        String EDIT = "${application}:system:dictionary:edit";
+        String REMOVE = "${application}:system:dictionary:remove";
+        String ENABLE = "${application}:system:dictionary:enable";
+        String DISABLE = "${application}:system:dictionary:disable";
+    }
+}
